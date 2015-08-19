1ed1155f8adf21e7330ba5d51da6bdbd7636333f
VRciF <none@none>
2015-08-20 00:40:08 +0200

    recreated amalgamation

--- procfuse-amalgamation.c
+++ procfuse-amalgamation.c
@@ -747,8 +747,23 @@ HashTableValue hash_table_iter_next(HashTableIterator *iterator)
 
 
 /*
- * procfuse.c
- *
+    ProcFuse is a C library which can be used to register string paths
+    representing a file of your own filesystem like /proc used by *nix
+    Copyright (C) 2015 - vrcif0@gmail.com
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 
--- procfuse-amalgamation.h
+++ procfuse-amalgamation.h
@@ -523,10 +523,25 @@ HashTableValue hash_table_iter_next(HashTableIterator *iterator);
 #endif /* #ifndef ALGORITHM_HASH_TABLE_H */
 
 
-/*
- * procfuse.h
- *
- */
+/*  
+    ProcFuse is a C library which can be used to register string paths
+    representing a file of your own filesystem like /proc used by *nix
+    Copyright (C) 2015 - vrcif0@gmail.com
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #ifndef PROCFUSE_H_
 #define PROCFUSE_H_
